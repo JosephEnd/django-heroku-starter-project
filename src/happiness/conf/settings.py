99f627b431704f069b258f0582d275cecef02cd2
JosephEnd <j.endruweit@student.fontys.nl>
2019-12-03 16:19:56 +0100

    [ 169499527 ] - Added whitenoise as middleware in 'src/happiness/conf/settings.py
    - Added staticfiles_storage to 'src/happiness/conf/settings.py

--- src/happiness/conf/settings.py
+++ src/happiness/conf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -58,6 +59,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "conf.wsgi.application"
 
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # ...
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
@@ -104,6 +111,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = "/static/"
 
