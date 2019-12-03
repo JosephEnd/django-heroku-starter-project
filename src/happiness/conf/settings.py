1d173f399c8ac08bb3bc1b96406b51aa58f8d311
JosephEnd <j.endruweit@student.fontys.nl>
2019-12-03 17:09:05 +0100

    [ 169499527 ] - Corrected Database section in 'src/happiness/conf/settings.py - Removed urlpattern, add again later. - python manage.py migrate succesful locally

--- src/happiness/conf/settings.py
+++ src/happiness/conf/settings.py
@@ -62,6 +62,13 @@ WSGI_APPLICATION = "conf.wsgi.application"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # ...
 ]
 
@@ -72,7 +79,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "happinessdb",
-        "USER": "happUser",
+        "USER": "happuser",
         "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "",
