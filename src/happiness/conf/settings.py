8c0862561b2ef125c227165a70ab312c0a89acd2
JosephEnd <j.endruweit@student.fontys.nl>
2019-12-11 11:28:59 +0100

    [ 169499527 ] Remove .env file from .gitignore

--- src/happiness/conf/settings.py
+++ src/happiness/conf/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = ",^NqzcYF'#V3jh(NqvJZ*gm28&^4YE{C8?<78MCB{*z'XE?o]}"
 
 SECRET_KEY = os.getenv("SECRET_KEY")
