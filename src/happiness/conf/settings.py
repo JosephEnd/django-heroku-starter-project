f8821a5dcf22363c80ab3bce8bea4da87274f86c
JosephEnd <j.endruweit@student.fontys.nl>
2019-12-11 09:50:34 +0100

    [ 169499527 ] Remove import django-heroku Remove django_heroku.settings in 'settings.py'

--- src/happiness/conf/settings.py
+++ src/happiness/conf/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -126,6 +125,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Heroku ssl
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
