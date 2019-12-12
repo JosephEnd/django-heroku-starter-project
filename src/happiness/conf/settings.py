461aef38c452b4025fbb3283c3a1470d66ebb612
JosephEnd <j.endruweit@student.fontys.nl>
2019-12-12 16:27:52 +0100

    [ 169499638 ] TEMPLATE_DIR

--- src/happiness/conf/settings.py
+++ src/happiness/conf/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Template dir inside the project
-TEMPLATE_DIRS = os.path.join(BASE_DIR, "../templates")
+TEMPLATE_DIRS = os.path.join(BASE_DIR, "../../templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
