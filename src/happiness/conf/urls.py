985a850cdd8ae35800ae69a5cdaffdf7680f9b1d
JosephEnd <j.endruweit@student.fontys.nl>
2019-12-12 15:03:56 +0100

    [ 169499638 ] url test

--- src/happiness/conf/urls.py
+++ src/happiness/conf/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("./app/happinessmeter/", include("happinessmeter.url")),
+    path("./app/happinessmeter", include("happinessmeter.url")),
     path("admin/", admin.site.urls),
 ]
