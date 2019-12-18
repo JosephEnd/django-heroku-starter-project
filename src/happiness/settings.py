cce6ec31d15e9a2f73491a40e6e714f1664d21cf
JosephEnd <j.endruweit@student.fontys.nl>
2019-12-18 09:52:26 +0100

    [ 169499638 ] Add 'hello,world' to rating template

--- src/happiness/settings.py
+++ src/happiness/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
+
 # -----------------------------------------------------------------------
 # Basic Config
 # -----------------------------------------------------------------------
@@ -69,8 +70,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Middleware Config
-    "materialize",
     # User apps
     "rating.apps.RatingConfig",
 ]
